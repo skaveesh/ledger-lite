7b09fb875bfe6664e2fb051cfc6e66f0342e2a07
Samintha Kaveesh <skaveesh@gmail.com>
2025-11-26 17:28:22 +1300

    add HTTP server skeleton

--- cmd/api-server/main.go
+++ cmd/api-server/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("LedgerLite")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("LedgerLite"))
+	})
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	log.Println("api server listening on :8080")
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("server error: %v", err)
+	}
 }
