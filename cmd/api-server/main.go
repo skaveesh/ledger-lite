cc1fffb2fae2335570d69ef8eef5c361be115204
Samintha Kaveesh <skaveesh@gmail.com>
2025-11-27 17:29:24 +1300

    add /categories GET

--- cmd/api-server/main.go
+++ cmd/api-server/main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+
+	"github.com/skaveesh/ledger-lite/internal/store"
 )
 
-func newRouter() http.Handler {
+type api struct {
+	store store.Store
+}
+
+func newAPI() *api {
+	return &api{store: store.NewMemoryStore()}
+}
+
+func (a *api) router() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
 		w.WriteHeader(http.StatusOK)
@@ -15,13 +26,25 @@ func newRouter() http.Handler {
 		w.WriteHeader(http.StatusOK)
 		_, _ = w.Write([]byte("ok"))
 	})
+	mux.HandleFunc("/categories", a.handleCategories)
 	return mux
 }
 
+func (a *api) handleCategories(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	_ = json.NewEncoder(w).Encode(a.store.ListCategories())
+}
+
 func main() {
+	app := newAPI()
 	server := &http.Server{
 		Addr:    ":8080",
-		Handler: newRouter(),
+		Handler: app.router(),
 	}
 
 	log.Println("api server listening on :8080")
